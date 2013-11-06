6c74aba121e020d6b6e79aa4b06ca72df80b222c
AfterLifeLochie <afterlifelochie@afterlifelochie.net>
2013-11-06 12:29:20 +1100

    Change Info to BuildInfo, make BuildInfo tokenified, change ant
    Changed ant script to import Ant constants into source-code.

---
+++ src/gcewing/sg/BuildInfo.java
@@ -0,0 +1,10 @@
+package gcewing.sg;
+
+public class BuildInfo {
+	public static final String modName = "Delta SGCraft";
+	public static final String modID = "SGCraft";
+	
+	public static final String versionNumber = "@VERSION@";
+	public static final String buildNumber = "@BUILD@";
+	
+}
