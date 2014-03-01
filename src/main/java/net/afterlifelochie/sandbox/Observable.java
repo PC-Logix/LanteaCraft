dbd1a731ee39525a855c2b8049992eeec5bf8b00
AfterLifeLochie <afterlifelochie@afterlifelochie.net>
2014-03-01 11:33:27 +1100

    Blargh.

--- src/main/java/net/afterlifelochie/sandbox/Observable.java
+++ src/main/java/net/afterlifelochie/sandbox/Observable.java
@@ -1,24 +1,48 @@
 package net.afterlifelochie.sandbox;
 
+/**
+ * Observable is a container for objects which have values for which we care
+ * about the contents of, but do not want to cache the old/new values in a
+ * non-persistent or non-volatile way.
+ * 
+ * @author AfterLifeLochie
+ * 
+ */
 public class Observable {
 
+	/**
+	 * The modified state.
+	 */
 	private volatile boolean modified;
+	/**
+	 * The parent Observable, in the event we are in a List or want to upwardly
+	 * notify Observable parent objects.
+	 */
 	private volatile Observable parent;
 
 	public Observable(Observable parent) {
 		this.parent = parent;
 	}
 
+	/**
+	 * Marks this object as modified. Only accessible to the extending object.
+	 */
 	protected void modify() {
 		this.modified = true;
 		if (this.parent != null)
 			this.parent.modify();
 	}
 
+	/**
+	 * Returns the state of this Observable.
+	 */
 	public boolean modified() {
 		return this.modified;
 	}
 
+	/**
+	 * Resets the state of this Observable.
+	 */
 	public void clearModified() {
 		this.modified = false;
 	}
--- src/main/java/net/afterlifelochie/sandbox/WatchedList.java
+++ src/main/java/net/afterlifelochie/sandbox/WatchedList.java
@@ -2,42 +2,78 @@ package net.afterlifelochie.sandbox;
 
 import java.util.HashMap;
 
+/**
+ * Represents a list with assigned key-value pairs which is Observable.
+ * 
+ * @author AfterLifeLochie
+ * 
+ * @param <A>
+ *            The type A of keys.
+ * @param <B>
+ *            The type B of values.
+ */
 public class WatchedList<A, B> extends Observable {
 
+	/**
+	 * The map of type A keys with type B values.
+	 */
 	private HashMap<A, B> values;
 
+	/**
+	 * Creates a new WatchedList.
+	 */
 	public WatchedList() {
 		super(null);
 		this.values = new HashMap<A, B>();
 	}
 
+	/**
+	 * Creates a new WatchedList with an Observable parent.
+	 * 
+	 * @param parent
+	 *            The parent Observable object.
+	 */
 	public WatchedList(Observable parent) {
 		super(parent);
 		this.values = new HashMap<A, B>();
 	}
-	
+
+	/**
+	 * @see {@link HashMap#get(Object)}
+	 */
 	public B get(A key) {
 		return values.get(key);
 	}
-	
+
+	/**
+	 * @see {@link HashMap#put(Object, Object)}
+	 */
 	public B set(A key, B value) {
 		this.modify();
 		return values.put(key, value);
 	}
-	
+
+	/**
+	 * @see {@link HashMap#size()}
+	 */
 	public int size() {
 		return values.size();
 	}
-	
+
+	/**
+	 * @see {@link HashMap#remove(Object)}
+	 */
 	public B remove(A key) {
 		this.modify();
 		return values.remove(key);
 	}
-	
+
+	/**
+	 * @see {@link HashMap#clear()}
+	 */
 	public void clear() {
 		this.modify();
 		values.clear();
 	}
-	
 
 }
