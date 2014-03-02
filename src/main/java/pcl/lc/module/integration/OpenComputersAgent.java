db9a8ccd9af243adb92832c9bb143755e1c16bd8
AfterLifeLochie <afterlifelochie@afterlifelochie.net>
2014-03-02 20:19:48 +1100

    Allow Block.StargateBase and Block.NaquadahGenerator to behave as ManagedEnvironment.

--- src/main/java/pcl/lc/module/integration/OpenComputersAgent.java
+++ src/main/java/pcl/lc/module/integration/OpenComputersAgent.java
@@ -1,5 +1,23 @@
 package pcl.lc.module.integration;
 
-public class OpenComputersAgent {
+import pcl.lc.LanteaCraft;
+import net.minecraft.world.World;
+import li.cil.oc.api.driver.Block;
+import li.cil.oc.api.network.ManagedEnvironment;
+
+public class OpenComputersAgent implements Block {
+
+	@Override
+	public boolean worksWith(World world, int x, int y, int z) {
+		int id = world.getBlockId(x, y, z);
+		return (id == LanteaCraft.Blocks.stargateBaseBlock.blockID)
+				|| (id == LanteaCraft.Blocks.naquadahGenerator.blockID);
+	}
+
+	@Override
+	public ManagedEnvironment createEnvironment(World world, int x, int y, int z) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
---
+++ src/main/java/pcl/lc/module/integration/OpenComputersWrapperPool.java
@@ -0,0 +1,6 @@
+package pcl.lc.module.integration;
+
+
+public class OpenComputersWrapperPool {
+
+}
