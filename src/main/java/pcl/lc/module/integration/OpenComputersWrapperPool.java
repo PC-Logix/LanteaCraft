fdbbf6cf7aa2a49449348b33c5bfa46fed8e2dbb
MichiyoRavencroft <Michiyo.Ravencroft@gmail.com>
2014-03-02 06:43:33 -0600

    Fixed node saving in OC
    lua
    sg = component.stargate
    sg.dial("BAAMAAP")
    sg.disconnect()

--- src/main/java/pcl/lc/module/integration/OpenComputersWrapperPool.java
+++ src/main/java/pcl/lc/module/integration/OpenComputersWrapperPool.java
@@ -23,14 +23,14 @@ public class OpenComputersWrapperPool {
 
 		private final IStargateAccess access;
 		private EnumStargateState stateWatcher;
-		protected Node node;
+		protected Node node = Network.newNode(this, Visibility.Network).withComponent(getComponentName()).create();
 		public StargateAccessWrapper(IStargateAccess access) {
 			this.access = access;
 		}
 
 		@Override
 		public Node node() {
-			return Network.newNode(this, Visibility.Network).withComponent(getComponentName()).create();
+			return node;
 		}
 
 		@Override
@@ -48,23 +48,23 @@ public class OpenComputersWrapperPool {
 		@Override
 		public void onMessage(Message message) {
 			// TODO Auto-generated method stub
-			
+
 		}
 
-		
-	    @Override
-	    public void load(final NBTTagCompound nbt) {
-	        if (node != null) node.load(nbt.getCompoundTag("node"));
-	    }
 
-	    @Override
-	    public void save(final NBTTagCompound nbt) {
-	        if (node != null) {
-	            final NBTTagCompound nodeTag = new NBTTagCompound();
-	            node.save(nodeTag);
-	            nbt.setCompoundTag("node", nodeTag);
-	        }
-	    }
+		@Override
+		public void load(final NBTTagCompound nbt) {
+			if (node != null) node.load(nbt.getCompoundTag("node"));
+		}
+
+		@Override
+		public void save(final NBTTagCompound nbt) {
+			if (node != null) {
+				final NBTTagCompound nodeTag = new NBTTagCompound();
+				node.save(nodeTag);
+				nbt.setCompoundTag("node", nodeTag);
+			}
+		}
 
 		@Override
 		public boolean canUpdate() {
@@ -82,34 +82,34 @@ public class OpenComputersWrapperPool {
 			return "stargate";
 		}
 
-		
+
 		//Our callbacks, aka the methods OC can call against a stargate.
-		
+
 		@Callback
 		public Object[] greet(Context context, Arguments args) {
-			   return new Object[]{String.format("Hello, %s!", args.checkString(0))};
-        }
-		
+			return new Object[]{String.format("Hello, %s!", args.checkString(0))};
+		}
+
 		@Callback
 		public Object[] dial(Context context, Arguments args) {
-				String address = args.checkString(0).toString().toUpperCase();
-				if (address.length() != 7 && address.length() != 9)
-					return new Object[]{"Stargate addresses must be 7 or 9 characters."};
-				else if (address == access.getLocalAddress() || address == access.getLocalAddress().substring(0, 7))
-					return new Object[]{"Stargate cannot connect to itself."};
-				else if (!access.connect(address))
-					return new Object[]{"Stargate cannot dial now."};
-				return new Object[] { true };
-        }
-		
+			String address = args.checkString(0).toString().toUpperCase();
+			if (address.length() != 7 && address.length() != 9)
+				return new Object[]{"Stargate addresses must be 7 or 9 characters."};
+			else if (address == access.getLocalAddress() || address == access.getLocalAddress().substring(0, 7))
+				return new Object[]{"Stargate cannot connect to itself."};
+			else if (!access.connect(address))
+				return new Object[]{"Stargate cannot dial now."};
+			return new Object[] { true };
+		}
+
 		@Callback
 		public Object[] disconnect(Context context, Arguments args) {
 			if (!access.disconnect())
 				return new Object[]{"Stargate cannot be closed from this end"};
 			return new Object[] { true };
-        }
-		
-		
+		}
+
+
 	}
 
 	public static class NaquadahGeneratorAccessWrapper implements IHookManagedEnvironment {
