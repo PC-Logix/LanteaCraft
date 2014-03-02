0b19913dbcbd1eede9afd709b060989765b9a693
AfterLifeLochie <afterlifelochie@afterlifelochie.net>
2014-03-02 20:16:32 +1100

    OpenComputers hook.

--- src/main/java/pcl/lc/module/ModuleIntegration.java
+++ src/main/java/pcl/lc/module/ModuleIntegration.java
@@ -7,10 +7,12 @@ import dan200.computer.api.ComputerCraftAPI;
 import pcl.lc.api.internal.IModule;
 import pcl.lc.core.ModuleManager.Module;
 import pcl.lc.module.integration.ComputerCraftAgent;
+import pcl.lc.module.integration.OpenComputersAgent;
 
 public class ModuleIntegration implements IModule {
 
 	private ComputerCraftAgent integrationComputerCraftAgent;
+	private OpenComputersAgent integrationOpenComputersAgent;
 
 	@Override
 	public Set<Module> getDependencies() {
@@ -31,6 +33,7 @@ public class ModuleIntegration implements IModule {
 	@Override
 	public void init() {
 		integrationComputerCraftAgent = new ComputerCraftAgent();
+		integrationOpenComputersAgent = new OpenComputersAgent();
 	}
 
 	@Override
---
+++ src/main/java/pcl/lc/module/integration/OpenComputersAgent.java
@@ -0,0 +1,5 @@
+package pcl.lc.module.integration;
+
+public class OpenComputersAgent {
+
+}
