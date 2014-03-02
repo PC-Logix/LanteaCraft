d130b054d4626ebe530082c63b84e10a512952b9
AfterLifeLochie <afterlifelochie@afterlifelochie.net>
2014-03-02 20:33:08 +1100

    Agent complete. Michiyo will have to fill in the blanks.
    I don't understand how OpenComputers is supposed to function, and the
    documentation doesn't provide enough info  for me to figure it out
    myself.

--- src/main/java/pcl/lc/module/integration/OpenComputersAgent.java
+++ src/main/java/pcl/lc/module/integration/OpenComputersAgent.java
@@ -1,6 +1,8 @@
 package pcl.lc.module.integration;
 
 import pcl.lc.LanteaCraft;
+import pcl.lc.api.INaquadahGeneratorAccess;
+import pcl.lc.api.IStargateAccess;
 import net.minecraft.world.World;
 import li.cil.oc.api.driver.Block;
 import li.cil.oc.api.network.ManagedEnvironment;
@@ -16,8 +18,15 @@ public class OpenComputersAgent implements Block {
 
 	@Override
 	public ManagedEnvironment createEnvironment(World world, int x, int y, int z) {
-		// TODO Auto-generated method stub
-		return null;
+		int id = world.getBlockId(x, y, z);
+		if (id == LanteaCraft.Blocks.stargateBaseBlock.blockID) {
+			IStargateAccess base = (IStargateAccess) world.getBlockTileEntity(x, y, z);
+			return new OpenComputersWrapperPool.StargateAccessWrapper(base);
+		} else if (id == LanteaCraft.Blocks.naquadahGenerator.blockID) {
+			INaquadahGeneratorAccess generator = (INaquadahGeneratorAccess) world.getBlockTileEntity(x, y, z);
+			return new OpenComputersWrapperPool.NaquadahGeneratorAccessWrapper(generator);
+		} else
+			throw new RuntimeException("Driver.Block handler specified invalid typeof!");
 	}
 
 }
--- src/main/java/pcl/lc/module/integration/OpenComputersWrapperPool.java
+++ src/main/java/pcl/lc/module/integration/OpenComputersWrapperPool.java
@@ -1,6 +1,141 @@
 package pcl.lc.module.integration;
 
+import pcl.lc.api.EnumStargateState;
+import pcl.lc.api.INaquadahGeneratorAccess;
+import pcl.lc.api.IStargateAccess;
+import net.minecraft.nbt.NBTTagCompound;
+import li.cil.oc.api.Network;
+import li.cil.oc.api.network.ManagedEnvironment;
+import li.cil.oc.api.network.Message;
+import li.cil.oc.api.network.Node;
+import li.cil.oc.api.network.Visibility;
 
 public class OpenComputersWrapperPool {
+
+	private interface IHookManagedEnvironment extends ManagedEnvironment {
+		public String getComponentName();
+	}
+
+	public static class StargateAccessWrapper implements IHookManagedEnvironment {
+
+		private final IStargateAccess access;
+		private EnumStargateState stateWatcher;
+
+		public StargateAccessWrapper(IStargateAccess access) {
+			this.access = access;
+		}
+
+		@Override
+		public Node node() {
+			return Network.newNode(this, Visibility.Network).withComponent(getComponentName()).create();
+		}
+
+		@Override
+		public void onConnect(Node node) {
+			// TODO Auto-generated method stub
+		}
+
+		@Override
+		public void onDisconnect(Node node) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void onMessage(Message message) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void load(NBTTagCompound nbt) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void save(NBTTagCompound nbt) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public boolean canUpdate() {
+			return true;
+		}
+
+		@Override
+		public void update() {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public String getComponentName() {
+			return "stargate";
+		}
+
+	}
+
+	public static class NaquadahGeneratorAccessWrapper implements IHookManagedEnvironment {
+
+		private final INaquadahGeneratorAccess access;
+		private EnumStargateState stateWatcher;
+
+		public NaquadahGeneratorAccessWrapper(INaquadahGeneratorAccess access) {
+			this.access = access;
+		}
+
+		@Override
+		public Node node() {
+			return Network.newNode(this, Visibility.Network).withComponent(getComponentName()).create();
+		}
+
+		@Override
+		public void onConnect(Node node) {
+			// TODO Auto-generated method stub
+		}
+
+		@Override
+		public void onDisconnect(Node node) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void onMessage(Message message) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void load(NBTTagCompound nbt) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void save(NBTTagCompound nbt) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public boolean canUpdate() {
+			return true;
+		}
+
+		@Override
+		public void update() {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public String getComponentName() {
+			return "naquadah_generator";
+		}
+
+	}
 
 }
