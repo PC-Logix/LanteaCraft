092b23c894727f092da999caa4b24afa1f064eb9
AfterLifeLochie <afterlifelochie@afterlifelochie.net>
2014-03-23 17:56:52 +1100

    Adding decor-glass.

---
+++ src/main/java/pcl/lc/blocks/BlockLanteaDecorGlass.java
@@ -0,0 +1,66 @@
+package pcl.lc.blocks;
+
+import java.util.List;
+
+import pcl.lc.LanteaCraft;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Icon;
+
+public class BlockLanteaDecorGlass extends Block {
+
+	private Icon missing;
+	private Icon lanteaGlassDefault;
+
+	public BlockLanteaDecorGlass(int id) {
+		super(id, Material.glass);
+	}
+
+	@Override
+	public boolean isOpaqueCube() {
+		return false;
+	}
+
+	@Override
+	public boolean renderAsNormalBlock() {
+		return false;
+	}
+
+	@Override
+	public int getRenderBlockPass() {
+		return 0;
+	}
+
+	@Override
+	public void registerIcons(IconRegister register) {
+		lanteaGlassDefault = register.registerIcon(LanteaCraft.getAssetKey() + ":lantean_glass_"
+				+ LanteaCraft.getProxy().getRenderMode());
+
+		missing = register.registerIcon(LanteaCraft.getAssetKey() + ":missing");
+	}
+
+	@Override
+	public Icon getIcon(int side, int data) {
+		switch (data) {
+		case 1:
+			return lanteaGlassDefault;
+		default:
+			return missing;
+		}
+	}
+
+	@Override
+	public void getSubBlocks(int itemID, CreativeTabs tab, List list) {
+		for (int i = 0; i < 1; i++)
+			list.add(new ItemStack(itemID, 1, i + 1));
+	}
+
+	@Override
+	public int damageDropped(int par1) {
+		return par1;
+	}
+
+}
