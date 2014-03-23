b8835afc5313e3de433426e31ea4e9cf83d505fb
AfterLifeLochie <afterlifelochie@afterlifelochie.net>
2014-03-23 17:46:41 +1100

    Adding ITransportRingAccess and ITransportRingHost (API).

---
+++ src/main/java/pcl/lc/api/ITransportRingAccess.java
@@ -0,0 +1,16 @@
+/**
+ * This file is part of the official LanteaCraft API. Please see the usage guide and
+ * restrictions on use in the package-info file.
+ */
+package pcl.lc.api;
+
+/**
+ * Provides an interface for external code to interact with Transport Ring
+ * structures. You should avoid interacting with Transport Rings outside of this
+ * API.
+ * 
+ * @author AfterLifeLochie
+ */
+public class ITransportRingAccess {
+
+}
