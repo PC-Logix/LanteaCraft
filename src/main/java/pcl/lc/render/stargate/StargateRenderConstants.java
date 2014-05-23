f69c6d4c2f84744b83efd211053cf7c3e37d3afe
Caitlyn Mainer <Michiyo.Ravencroft@gmail.com>
2014-05-23 18:50:51 -0500

    Fixes chevron locking order from #161

--- src/main/java/pcl/lc/render/stargate/StargateRenderConstants.java
+++ src/main/java/pcl/lc/render/stargate/StargateRenderConstants.java
@@ -39,7 +39,7 @@ public class StargateRenderConstants {
 	public final static int chevronTextureIndex = 0x05;
 	public final static int chevronLitTextureIndex = 0x16;
 
-	public final static int[] standardRenderQueue = { -1, 1, 2, 3, 6, 7, 8, 0 };
-	public final static int[] extendedRenderQueue = { -1, 1, 2, 3, 4, 5, 6, 7, 8, 0 };
+	public final static int[] standardRenderQueue = { -1, 8 ,7, 6, 3, 2, 1, 0 };
+	public final static int[] extendedRenderQueue = { -1, 8 ,7, 6, 3, 2, 1, 5, 4, 0 };
 
 }
