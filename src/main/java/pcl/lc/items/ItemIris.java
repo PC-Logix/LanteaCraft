23d460c49a95148f2dfffdcbedd3c98a3e659ff4
AfterLifeLochie <afterlifelochie@afterlifelochie.net>
2014-05-24 11:21:01 +1000

    Fixing ItemIris not showing correct icons

--- src/main/java/pcl/lc/items/ItemIris.java
+++ src/main/java/pcl/lc/items/ItemIris.java
@@ -6,13 +6,19 @@ import pcl.lc.LanteaCraft;
 import pcl.lc.api.EnumIrisType;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.Icon;
 
 public class ItemIris extends Item {
+
+	private Icon iconMissing;
+	private Icon iconMechanical;
+	private Icon iconEnergy;
 
 	public ItemIris(int id) {
 		super(id);
@@ -33,13 +39,31 @@ public class ItemIris extends Item {
 	}
 
 	@Override
-	@SideOnly(Side.CLIENT)
-	protected String getIconString() {
-		return LanteaCraft.getAssetKey() + ":energy_crystal_" + LanteaCraft.getProxy().getRenderMode();
+	public void registerIcons(IconRegister reg) {
+		this.iconMissing = reg.registerIcon(LanteaCraft.getAssetKey() + ":missing");
+		this.iconMechanical = reg.registerIcon(LanteaCraft.getAssetKey() + ":energy_iris_upgrade_"
+				+ LanteaCraft.getProxy().getRenderMode());
+		this.iconEnergy = reg.registerIcon(LanteaCraft.getAssetKey() + ":iris_upgrade_"
+				+ LanteaCraft.getProxy().getRenderMode());
 	}
 
-	public void acceptDamage(double quantity) {
+	@Override
+	public Icon getIconIndex(ItemStack par1ItemStack) {
+		EnumIrisType typeof = getType(par1ItemStack);
+		if (typeof != null)
+			switch (typeof) {
+			case ENERGY:
+				return iconEnergy;
+			case MECHANICAL:
+				return iconMechanical;
+			}
+		return iconMissing;
+	}
 
+	public void acceptDamage(ItemStack stack, double quantity) {
+		EnumIrisType typeof = getType(stack);
+		if (!typeof.isInvulnerable())
+			setIrisDamage(stack, getIrisDamage(stack) + quantity);
 	}
 
 	public double getMaximumDamage(EnumIrisType typeof) {
@@ -70,23 +94,23 @@ public class ItemIris extends Item {
 	public double getIrisDamage(ItemStack stack) {
 		if (stack.stackTagCompound == null)
 			stack.setTagCompound(new NBTTagCompound());
-		if (!stack.stackTagCompound.hasKey("stored-energy"))
+		if (!stack.stackTagCompound.hasKey("damage"))
 			return 0.0d;
-		return stack.stackTagCompound.getDouble("stored-energy");
+		return stack.stackTagCompound.getDouble("damage");
 	}
 
 	public void setIrisDamage(ItemStack stack, double value) {
 		if (stack.stackTagCompound == null)
 			stack.setTagCompound(new NBTTagCompound());
-		stack.stackTagCompound.setDouble("stored-energy", value);
+		stack.stackTagCompound.setDouble("damage", value);
 		updateDisplay(stack);
 	}
 
 	@Override
 	public void addInformation(ItemStack stack, EntityPlayer player, List list, boolean wat) {
-		double energy = ((100 * getIrisDamage(stack)) / getMaximumDamage(getType(stack)));
+		double damage = ((100 * getIrisDamage(stack)) / getMaximumDamage(getType(stack)));
 		list.add(String.format("Type: %s", getType(stack).getName()));
-		list.add(String.format("Damage: %.2f%%", energy));
+		list.add(String.format("Damage: %.2f%%", damage));
 	}
 
 	private void updateDisplay(ItemStack stack) {
