47db4d7af2d23e5ce299a1c8592d72568fd8fec5
AfterLifeLochie <afterlifelochie@afterlifelochie.net>
2014-05-24 11:22:05 +1000

    Iris controller stub

---
+++ src/main/java/pcl/lc/items/ItemIrisController.java
@@ -0,0 +1,12 @@
+package pcl.lc.items;
+
+import net.minecraft.item.Item;
+
+public class ItemIrisController extends Item {
+
+	public ItemIrisController(int id) {
+		super(id);
+		// TODO Auto-generated constructor stub
+	}
+
+}
