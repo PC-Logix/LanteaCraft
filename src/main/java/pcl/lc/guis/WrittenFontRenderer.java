43346ce4ec1f9eb6f18b795b553f4ec40499ce42
AfterLifeLochie <afterlifelochie@afterlifelochie.net>
2014-05-25 15:39:18 +1000

    Lineboxing is annoying.

--- src/main/java/pcl/lc/guis/WrittenFontRenderer.java
+++ src/main/java/pcl/lc/guis/WrittenFontRenderer.java
@@ -2,8 +2,10 @@ package pcl.lc.guis;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Stack;
 import java.util.logging.Level;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -71,13 +73,22 @@ public class WrittenFontRenderer {
 	public static class PageBox {
 		public final int page_width, page_height;
 		public final int margin_left, margin_right;
+		public final int min_space_size;
 		public LinkedList<BoxedLine> lines = new LinkedList<BoxedLine>();
 
-		public PageBox(int w, int h, int ml, int mr) {
+		public PageBox(int w, int h, int ml, int mr, int min_sp) {
 			this.page_width = w;
 			this.page_height = h;
 			this.margin_left = ml;
 			this.margin_right = mr;
+			this.min_space_size = min_sp;
+		}
+
+		public int getFreeHeight() {
+			int h = page_height;
+			for (BoxedLine line : lines)
+				h -= line.line_height;
+			return h;
 		}
 
 	}
@@ -155,6 +166,106 @@ public class WrittenFontRenderer {
 				.log(Level.INFO,
 						String.format("Read %s metric definitions from file %s", char_metrics.size(),
 								fontMetricName.toString()));
+	}
+
+	/**
+	 * Attempt to box a line or part of a line onto a PageBox. This immediately
+	 * attempts to fit as much of the line onto a LineBox and then glues it to
+	 * the tail of a PageBox if the PageBox can support the addition of a line.
+	 * Any overflow text which cannot be boxed onto the page is returned.
+	 * 
+	 * @param line
+	 *            The line
+	 * @param page
+	 *            The page to box onto
+	 * @param isBeginParagraph
+	 *            If this line is the beginning of a paragraph section
+	 * @return Any overflow text, or null if no overflow occurs
+	 */
+	private String boxLine(String text, PageBox page, boolean isBeginParagraph) {
+		// Calculate some required properties
+		int realPageWidth = page.page_width - page.margin_left - page.margin_right;
+		int freeHeight = page.getFreeHeight();
+
+		int width_new_line = 0, width_new_word = 0;
+		// Start globbing characters
+		Stack<String> words = new Stack<String>();
+		Stack<Character> chars = new Stack<Character>();
+		for (int i = 0, j = text.length(); i < j; i++) {
+			char c = text.charAt(i);
+			// Treat space as a word separator
+			if (c == ' ') {
+				// Push a whole word if one exists
+				if (chars.size() > 0) {
+					// Find out if there is enough space
+					int new_width_nl = width_new_line + width_new_word + page.min_space_size;
+					if (realPageWidth >= new_width_nl) {
+						// Yes, there is enough space, add the word
+						width_new_line += width_new_word; // Don't add a min-space
+						words.push(String.valueOf(chars.toArray(new Character[0])));
+						width_new_word = 0;
+					} else {
+						// No, the word doesn't fit, back it up
+						width_new_word = 0;
+						words.clear();
+					}
+				}
+			} else {
+				GlyphMetric metric = char_metrics.get((int) c);
+				if (metric != null) {
+					width_new_word += metric.width;
+					chars.push(c);
+				}
+			}
+		}
+		
+		// Glue the whole line together
+		StringBuilder line = new StringBuilder();
+		for (int i = 0; i < words.size(); i++) {
+			line.append(words.get(i));
+			if (i != words.size() - 1)
+				line.append(" ");
+		}
+		
+		// Find the maximum height of any characters in the line
+		int height_new_line = 0;
+		for (int i = 0; i < line.length(); i++) {
+			char c = line.charAt(i);
+			if (c != ' ') {
+				GlyphMetric metric = char_metrics.get((int) c);
+				if (metric.height > height_new_line)
+					height_new_line = metric.height;
+			}
+		}
+		
+		// If the line doesn't fit at all, we can't do anything
+		if (height_new_line > freeHeight)
+			return text;
+		
+		// Figure out how much space is left over from the line
+		int space_remain = realPageWidth - width_new_line;
+		int extra_px_per_space = (int) Math.floor(space_remain / words.size());
+		
+		// Create the linebox
+		BoxedLine linebox = new BoxedLine(line.toString(), page.min_space_size + extra_px_per_space, height_new_line);
+
+		return null;
+	}
+
+	/**
+	 * Attempt to box a paragraph or part of a paragraph onto a PageBox. This
+	 * immediately attempts to fit as much of the paragraph onto the page. Any
+	 * overflow text which cannot be boxed onto the page is returned.
+	 * 
+	 * @param text
+	 *            The paragraph
+	 * @param page
+	 *            The page to box onto
+	 * @return Any overflow text, or null if no overflow occurs
+	 */
+	private String boxParagraph(String text, PageBox page) {
+
+		return null;
 	}
 
 }
