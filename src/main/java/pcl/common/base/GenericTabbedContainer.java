bfc97eefa25e09c26366e87fdfb94a0c16ebdf3c
AfterLifeLochie <afterlifelochie@afterlifelochie.net>
2014-05-30 10:59:00 +1000

    GenericTabbedContainer stub

---
+++ src/main/java/pcl/common/base/GenericTabbedContainer.java
@@ -0,0 +1,24 @@
+package pcl.common.base;
+
+import net.minecraft.inventory.Container;
+
+public class GenericTabbedContainer extends GenericContainerGUI {
+
+	public GenericTabbedContainer(Container container, int width, int height) {
+		super(container, width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void drawBackgroundLayer(float partialTickCount, int mouseX, int mouseY) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void drawForegroundLayer(int mouseX, int mouseY) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
