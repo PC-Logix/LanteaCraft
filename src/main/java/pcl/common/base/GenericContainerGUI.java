48ba51022e123e41a1af3b1ea632e2bc6bf0bdf6
AfterLifeLochie <afterlifelochie@afterlifelochie.net>
2014-05-30 10:58:24 +1000

    Cleaning up generic container

--- src/main/java/pcl/common/base/GenericContainerGUI.java
+++ src/main/java/pcl/common/base/GenericContainerGUI.java
@@ -28,27 +28,9 @@ public abstract class GenericContainerGUI extends GuiContainer {
 		this(container, container.xSize, container.ySize);
 	}
 
-	public void drawFramedSymbols(int x, int y, String address) {
-		int symbolsPerRow = 9;
-		int scale = 2;
-		bindSGTexture("symbol_frame.png", 512 / scale, 128 / scale);
-		drawTexturedRect(x - (472 / scale) / 2, y, 472 / scale, 88 / scale, 0, 0);
-		GlyphRenderer.drawAddress(mc, address, x - (472 / scale) / 2, y, symbolsPerRow, scale, zLevel);
-	}
+	protected abstract void drawBackgroundLayer(float partialTickCount, int mouseX, int mouseY);
 
-	public void drawAddressString(int x, int y, String address, int len, String padding, String caret) {
-		StringBuilder result = new StringBuilder();
-		result.append(address);
-		if (len != result.length())
-			result.append(caret);
-		while (len > result.length())
-			result.append(padding);
-		drawCenteredString(fontRenderer, result.toString(), x, y, 0xffffff);
-	}
-
-	public void bindSGTexture(String name, int usize, int vsize) {
-		bindTexture(LanteaCraft.getResource("textures/gui/" + name), usize, vsize);
-	}
+	protected abstract void drawForegroundLayer(int mouseX, int mouseY);
 
 	@Override
 	public void drawScreen(int par1, int par2, float par3) {
@@ -63,17 +45,31 @@ public abstract class GenericContainerGUI extends GuiContainer {
 		drawBackgroundLayer(partialTickCount, mouseX, mouseY);
 	}
 
-	protected abstract void drawBackgroundLayer(float partialTickCount, int mouseX, int mouseY);
-
 	@Override
 	protected void drawGuiContainerForegroundLayer(int mouseX, int mouseY) {
 		drawForegroundLayer(mouseX, mouseY);
 	}
 
-	protected abstract void drawForegroundLayer(int mouseX, int mouseY);
-
 	protected void close() {
 		mc.thePlayer.closeScreen();
+	}
+	
+
+	public void drawFramedSymbols(int x, int y, String address) {
+		int scale = 2;
+		bindTexture(LanteaCraft.getResource("textures/gui/symbol_frame.png"), 512 / scale, 128 / scale);
+		drawTexturedRect(x - (472 / scale) / 2, y, 472 / scale, 88 / scale, 0, 0);
+		GlyphRenderer.drawAddress(mc, address, x - (472 / scale) / 2, y, 9, scale, zLevel);
+	}
+
+	public void drawAddressString(int x, int y, String address, int len, String padding, String caret) {
+		StringBuilder result = new StringBuilder();
+		result.append(address);
+		if (len != result.length() && caret != null)
+			result.append(caret);
+		while (len > result.length())
+			result.append(padding);
+		drawCenteredString(fontRenderer, result.toString(), x, y, 0xffffff);
 	}
 
 	protected void bindTexture(ResourceLocation rsrc) {
