1136bac3e3ad48f5aebb05c2beaba91ebc873df1
AfterLifeLochie <afterlifelochie@afterlifelochie.net>
2014-06-07 13:10:30 +1000

    Derp.

--- src/main/java/pcl/lc/module/integration/WailaAgent.java
+++ src/main/java/pcl/lc/module/integration/WailaAgent.java
@@ -1,10 +1,18 @@
 package pcl.lc.module.integration;
 
+import java.util.List;
+
+import net.minecraft.item.ItemStack;
+import mcp.mobius.waila.api.IWailaConfigHandler;
+import mcp.mobius.waila.api.IWailaDataAccessor;
+import mcp.mobius.waila.api.IWailaDataProvider;
+import mcp.mobius.waila.api.IWailaRegistrar;
+import cpw.mods.fml.common.event.FMLInterModComms;
 import pcl.lc.api.internal.Agent;
 import pcl.lc.api.internal.IIntegrationAgent;
 
 @Agent(modname = "Waila")
-public class WailaAgent implements IIntegrationAgent {
+public class WailaAgent implements IIntegrationAgent, IWailaDataProvider {
 
 	@Override
 	public String modName() {
@@ -13,8 +21,38 @@ public class WailaAgent implements IIntegrationAgent {
 
 	@Override
 	public void init() {
+		FMLInterModComms.sendMessage("Waila", "register", "pcl.lc.module.integration.WailaAgent.callbackRegister");
+	}
+
+	public static void callbackRegister(IWailaRegistrar registrar) {
 		// TODO Auto-generated method stub
-		
+	}
+
+	@Override
+	public ItemStack getWailaStack(IWailaDataAccessor accessor, IWailaConfigHandler config) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<String> getWailaHead(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor,
+			IWailaConfigHandler config) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<String> getWailaBody(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor,
+			IWailaConfigHandler config) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<String> getWailaTail(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor,
+			IWailaConfigHandler config) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
