c2168637889e43927cd20130fdfed5b5cee7aa94
AfterLifeLochie <afterlifelochie@afterlifelochie.net>
2014-08-28 16:28:28 +1000

    TileEntity stub a little less dumb.

--- src/main/java/lc/common/base/LCTile.java
+++ src/main/java/lc/common/base/LCTile.java
@@ -1,90 +1,101 @@
 package lc.common.base;
 
 import lc.common.network.IPacketHandler;
-import lc.common.network.LCPacket;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 
-public class LCTile extends TileEntity implements IInventory, IPacketHandler {
+public abstract class LCTile extends TileEntity implements IInventory, IPacketHandler {
+
+	public abstract IInventory getInventory();
+
+	public abstract void thinkClient();
+
+	public abstract void thinkServer();
 
 	@Override
 	public int getSizeInventory() {
-		// TODO Auto-generated method stub
-		return 0;
+		if (getInventory() == null)
+			return 0;
+		return getInventory().getSizeInventory();
 	}
 
 	@Override
 	public ItemStack getStackInSlot(int p_70301_1_) {
-		// TODO Auto-generated method stub
-		return null;
+		if (getInventory() == null)
+			return null;
+		return getInventory().getStackInSlot(p_70301_1_);
 	}
 
 	@Override
 	public ItemStack decrStackSize(int p_70298_1_, int p_70298_2_) {
-		// TODO Auto-generated method stub
-		return null;
+		if (getInventory() == null)
+			return null;
+		return getInventory().decrStackSize(p_70298_1_, p_70298_2_);
 	}
 
 	@Override
 	public ItemStack getStackInSlotOnClosing(int p_70304_1_) {
-		// TODO Auto-generated method stub
-		return null;
+		if (getInventory() == null)
+			return null;
+		return getInventory().getStackInSlotOnClosing(p_70304_1_);
 	}
 
 	@Override
 	public void setInventorySlotContents(int p_70299_1_, ItemStack p_70299_2_) {
-		// TODO Auto-generated method stub
-
+		if (getInventory() == null)
+			return;
+		getInventory().setInventorySlotContents(p_70299_1_, p_70299_2_);
 	}
 
 	@Override
 	public String getInventoryName() {
-		// TODO Auto-generated method stub
-		return null;
+		if (getInventory() == null)
+			return null;
+		return getInventory().getInventoryName();
 	}
 
 	@Override
 	public boolean hasCustomInventoryName() {
-		// TODO Auto-generated method stub
-		return false;
+		if (getInventory() == null)
+			return false;
+		return getInventory().hasCustomInventoryName();
 	}
 
 	@Override
 	public int getInventoryStackLimit() {
-		// TODO Auto-generated method stub
-		return 0;
+		if (getInventory() == null)
+			return 0;
+		return getInventory().getInventoryStackLimit();
 	}
 
 	@Override
 	public boolean isUseableByPlayer(EntityPlayer p_70300_1_) {
-		// TODO Auto-generated method stub
-		return false;
+		if (getInventory() == null)
+			return false;
+		return getInventory().isUseableByPlayer(p_70300_1_);
 	}
 
 	@Override
 	public void openInventory() {
-		// TODO Auto-generated method stub
-
+		if (getInventory() == null)
+			return;
+		getInventory().openInventory();
 	}
 
 	@Override
 	public void closeInventory() {
-		// TODO Auto-generated method stub
-
+		if (getInventory() == null)
+			return;
+		getInventory().closeInventory();
 	}
 
 	@Override
 	public boolean isItemValidForSlot(int p_94041_1_, ItemStack p_94041_2_) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public void handlePacket(LCPacket packetOf, EntityPlayer player) {
-		// TODO Auto-generated method stub
-
+		if (getInventory() == null)
+			return false;
+		return getInventory().isItemValidForSlot(p_94041_1_, p_94041_2_);
 	}
 
 }
