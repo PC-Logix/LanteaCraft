deeac2c65b3aa465a0df01bc9872af645e0b2fc5
AfterLifeLochie <afterlifelochie@afterlifelochie.net>
2014-11-01 23:15:26 +1100

    Graphics buffering.

---
+++ src/main/java/lc/client/opengl/BufferDisplayList.java
@@ -0,0 +1,62 @@
+package lc.client.opengl;
+
+import org.lwjgl.opengl.GL11;
+
+import lc.api.rendering.IGraphicsBuffer;
+
+public class BufferDisplayList implements IGraphicsBuffer {
+
+	private boolean assigned = false;
+	private boolean entered = false;
+	private boolean bound = false;
+	private int idx;
+
+	@Override
+	public void init() {
+		if (assigned)
+			throw new RuntimeException("Illegal state: cannot init() while assigned.");
+		idx = GL11.glGenLists(1);
+		assigned = true;
+	}
+
+	@Override
+	public void enter() {
+		if (entered)
+			throw new RuntimeException("Illegal state: cannot enter() while entered.");
+		GL11.glNewList(idx, GL11.GL_COMPILE);
+		entered = true;
+	}
+
+	@Override
+	public void exit() {
+		if (!entered)
+			throw new RuntimeException("Illegal state: cannot exit() while exited.");
+		GL11.glEnd();
+		GL11.glEndList();
+		entered = false;
+	}
+
+	@Override
+	public void bind(Object... args) {
+		if (bound)
+			throw new RuntimeException("Illegal state: cannot bind() while bound.");
+		GL11.glCallList(idx);
+		bound = true;
+	}
+
+	@Override
+	public void release() {
+		if (!bound)
+			throw new RuntimeException("Illegal state: cannot release() while released.");
+		bound = false;
+	}
+
+	@Override
+	public void delete() {
+		if (!assigned)
+			throw new RuntimeException("Illegal state: cannot delete() while not assigned.");
+		GL11.glDeleteLists(idx, 1);
+		assigned = false;
+	}
+
+}
---
+++ src/main/java/lc/client/opengl/BufferTexture.java
@@ -0,0 +1,90 @@
+package lc.client.opengl;
+
+import java.nio.ByteBuffer;
+
+import org.lwjgl.opengl.ARBFramebufferObject;
+import org.lwjgl.opengl.EXTFramebufferObject;
+import org.lwjgl.opengl.GL11;
+
+import lc.api.rendering.IGraphicsBuffer;
+
+public class BufferTexture implements IGraphicsBuffer {
+
+	private boolean assigned = false;
+	private boolean entered = false;
+	private boolean bound = false;
+
+	private int texture, fbo, depth;
+	private int width, height;
+
+	public BufferTexture(int width, int height) {
+		this.width = width;
+		this.height = height;
+	}
+
+	@Override
+	public void init() {
+		if (assigned)
+			throw new RuntimeException("Illegal state: cannot init() while assigned.");
+		fbo = EXTFramebufferObject.glGenFramebuffersEXT();
+		texture = GL11.glGenTextures();
+		depth = EXTFramebufferObject.glGenRenderbuffersEXT();
+		EXTFramebufferObject.glBindFramebufferEXT(EXTFramebufferObject.GL_FRAMEBUFFER_EXT, fbo);
+		GL11.glBindTexture(GL11.GL_TEXTURE_2D, texture);
+		GL11.glTexParameterf(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR);
+		GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGBA8, width, height, 0, GL11.GL_RGBA, GL11.GL_INT,
+				(ByteBuffer) null);
+		EXTFramebufferObject.glFramebufferTexture2DEXT(EXTFramebufferObject.GL_FRAMEBUFFER_EXT,
+				EXTFramebufferObject.GL_COLOR_ATTACHMENT0_EXT, GL11.GL_TEXTURE_2D, texture, 0);
+		EXTFramebufferObject.glBindRenderbufferEXT(EXTFramebufferObject.GL_RENDERBUFFER_EXT, depth);
+		EXTFramebufferObject.glRenderbufferStorageEXT(EXTFramebufferObject.GL_RENDERBUFFER_EXT,
+				ARBFramebufferObject.GL_DEPTH24_STENCIL8, width, height);
+		EXTFramebufferObject.glFramebufferRenderbufferEXT(EXTFramebufferObject.GL_FRAMEBUFFER_EXT,
+				ARBFramebufferObject.GL_DEPTH_STENCIL_ATTACHMENT, EXTFramebufferObject.GL_RENDERBUFFER_EXT, depth);
+		EXTFramebufferObject.glBindFramebufferEXT(EXTFramebufferObject.GL_FRAMEBUFFER_EXT, 0);
+		assigned = true;
+	}
+
+	@Override
+	public void enter() {
+		if (entered)
+			throw new RuntimeException("Illegal state: cannot enter() while entered.");
+		EXTFramebufferObject.glBindFramebufferEXT(EXTFramebufferObject.GL_FRAMEBUFFER_EXT, fbo);
+		entered = true;
+	}
+
+	@Override
+	public void exit() {
+		if (!entered)
+			throw new RuntimeException("Illegal state: cannot exit() while exited.");
+		EXTFramebufferObject.glBindFramebufferEXT(EXTFramebufferObject.GL_FRAMEBUFFER_EXT, 0);
+		entered = false;
+	}
+
+	@Override
+	public void bind(Object... args) {
+		if (bound)
+			throw new RuntimeException("Illegal state: cannot bind() while bound.");
+		GL11.glEnable(GL11.GL_TEXTURE_2D);
+		GL11.glBindTexture(GL11.GL_TEXTURE_2D, texture);
+		bound = true;
+	}
+
+	@Override
+	public void release() {
+		if (!bound)
+			throw new RuntimeException("Illegal state: cannot release() while released.");
+		bound = false;
+	}
+
+	@Override
+	public void delete() {
+		if (!assigned)
+			throw new RuntimeException("Illegal state: cannot delete() while not assigned.");
+		EXTFramebufferObject.glDeleteFramebuffersEXT(fbo);
+		GL11.glDeleteTextures(texture);
+		EXTFramebufferObject.glDeleteRenderbuffersEXT(depth);
+		assigned = false;
+	}
+
+}
