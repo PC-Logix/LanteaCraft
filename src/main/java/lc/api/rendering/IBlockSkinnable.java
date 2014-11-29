8ea45ab067b3bf81ac20e79fc4ffce8c1663bf48
Lochie <afterlifelochie@afterlifelochie.net>
2014-11-29 23:05:31 +1100

    Add setSkinBlock(Block; int) in contract.

--- src/main/java/lc/api/rendering/IBlockSkinnable.java
+++ src/main/java/lc/api/rendering/IBlockSkinnable.java
@@ -2,10 +2,38 @@ package lc.api.rendering;
 
 import net.minecraft.block.Block;
 
+/**
+ * Contract interface for tile entities which are capable of being 'painted'
+ * with other blocks.
+ * 
+ * @author AfterLifeLochie
+ *
+ */
 public interface IBlockSkinnable {
 
+	/**
+	 * Sets the skin block to the provided block and metadata.
+	 * 
+	 * @param block
+	 *            The block singleton.
+	 * @param metadata
+	 *            If the block has no metadata, 0 should be provided.
+	 */
+	public void setSkinBlock(Block block, int metadata);
+
+	/**
+	 * Get the current skin block singleton.
+	 * 
+	 * @return The current skin block singleton.
+	 */
 	public Block getSkinBlock();
 
+	/**
+	 * Get the current skin block metadata.
+	 * 
+	 * @return The current skin block metadata, or 0 if the block has no
+	 *         metadata.
+	 */
 	public int getSkinBlockMetadata();
 
 }
