b00ab5373d499c3bb3b98e65410f9b11470dadbc
Lochie <afterlifelochie@afterlifelochie.net>
2014-12-09 19:36:11 +1100

    More fixies.

--- src/main/java/lc/common/base/generation/scattered/LCScatteredFeatureGenerator.java
+++ src/main/java/lc/common/base/generation/scattered/LCScatteredFeatureGenerator.java
@@ -1,8 +1,12 @@
 package lc.common.base.generation.scattered;
 
+import java.lang.reflect.Constructor;
 import java.util.Random;
 
-import lc.generation.LanteaSurfaceStargate;
+import lc.api.defs.IStructureDefinition;
+import lc.common.LCLog;
+import lc.common.impl.registry.StructureRegistry;
+import lc.core.LCRuntime;
 import net.minecraft.world.gen.structure.MapGenStructure;
 import net.minecraft.world.gen.structure.StructureStart;
 
@@ -13,32 +17,46 @@ import net.minecraft.world.gen.structure.StructureStart;
  *
  */
 public final class LCScatteredFeatureGenerator extends MapGenStructure {
+	private final StructureRegistry registry;
 
-	private int maxStructDensity = 32;
-	private int minStructDensity = 8;
+	/** Default constructor */
+	public LCScatteredFeatureGenerator() {
+		registry = (StructureRegistry) LCRuntime.runtime.registries().structures();
+	}
 
 	@Override
 	public String func_143025_a() {
 		return "LanteaCraft";
 	}
 
-	@Override
-	protected boolean canSpawnStructureAtCoords(int i, int j) {
-		int iv = i, jv = j;
-		if (i < 0)
-			i -= maxStructDensity - 1;
-		if (j < 0)
-			j -= maxStructDensity - 1;
-		int imul = i, jmul = j;
-		Random var7 = worldObj.setRandomSeed(i / maxStructDensity, j / maxStructDensity, 14357617);
-		imul += var7.nextInt(maxStructDensity - minStructDensity);
-		jmul += var7.nextInt(maxStructDensity - minStructDensity);
-		return iv == imul && jv == jmul;
+	private IStructureDefinition findStructureStart(int x, int z) {
+		IStructureDefinition[] defs = registry.allDefs();
+		for (IStructureDefinition def : defs)
+			if (def.canGenerateAt(worldObj, rand, x, z))
+				return def;
+		return null;
 	}
 
 	@Override
-	protected StructureStart getStructureStart(int i, int j) {
-		return new LCScatteredFeatureStart(LanteaSurfaceStargate.class, worldObj, rand, i, j);
+	protected boolean canSpawnStructureAtCoords(int par1, int par2) {
+		return findStructureStart(par1, par2) != null;
+	}
+
+	@Override
+	protected StructureStart getStructureStart(int cx, int cz) {
+		IStructureDefinition def = findStructureStart(cx, cz);
+		if (def != null)
+			try {
+				Class<? extends StructureStart> start = def.getStructureClass();
+				Constructor<? extends StructureStart> ctr = start.getConstructor(Random.class, Integer.class,
+						Integer.class);
+				if (ctr != null)
+					return ctr.newInstance(rand, cx, cz);
+			} catch (Throwable t) {
+				LCLog.fatal("Couldn't initialize new structure start for type %s.", def.getName(), t);
+
+			}
+		return null;
 	}
 
 }
