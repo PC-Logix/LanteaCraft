ac5cf0b33910a67982af9a5d6f6425c93d7aed9a
Lochie <afterlifelochie@afterlifelochie.net>
2014-12-25 09:40:04 +1100

    Tidy up definition management registry.

--- src/main/java/lc/common/impl/registry/DefinitionRegistry.java
+++ src/main/java/lc/common/impl/registry/DefinitionRegistry.java
@@ -5,7 +5,6 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 
-import lc.BuildInfo;
 import lc.LCRuntime;
 import lc.api.components.IComponentRegistry;
 import lc.api.components.IDefinitionRegistry;
@@ -22,26 +21,13 @@ import lc.common.base.LCTileRenderer;
 import lc.common.util.LCCreativeTabManager;
 import net.minecraft.block.Block;
 import net.minecraft.client.renderer.entity.Render;
-import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.crafting.FurnaceRecipes;
-import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.WeightedRandomChestContent;
-import net.minecraftforge.common.ChestGenHooks;
-import net.minecraftforge.oredict.OreDictionary;
-import net.minecraftforge.oredict.ShapedOreRecipe;
-import net.minecraftforge.oredict.ShapelessOreRecipe;
-import cpw.mods.fml.client.registry.ClientRegistry;
-import cpw.mods.fml.client.registry.ISimpleBlockRenderingHandler;
 import cpw.mods.fml.client.registry.RenderingRegistry;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
-import cpw.mods.fml.common.registry.VillagerRegistry;
-import cpw.mods.fml.common.registry.VillagerRegistry.IVillageTradeHandler;
 
 /**
  * Global definition registry implementation.
@@ -118,21 +104,6 @@ public class DefinitionRegistry implements IDefinitionRegistry {
 							element.getComponentOwner());
 			} else
 				LCLog.warn("Strange definition type %s, ignoring it.", definition.getClass().getName());
-	}
-
-	/**
-	 * Register a Block with a given class and unlocalized name. The block will
-	 * use the default {@link ItemBlock} structure when held as an item. The
-	 * block will display in the default Creative tabs.
-	 *
-	 * @param classOf
-	 *            The class of the block.
-	 * @param unlocalizedName
-	 *            The unlocalized name.
-	 * @return The Block singleton.
-	 */
-	public <T extends Block> T registerBlock(Class<? extends T> classOf, String unlocalizedName) {
-		return registerBlock(classOf, ItemBlock.class, unlocalizedName);
 	}
 
 	/**
@@ -244,107 +215,6 @@ public class DefinitionRegistry implements IDefinitionRegistry {
 	}
 
 	/**
-	 * Registers a trade handler for a type of villager.
-	 *
-	 * @param villagerID
-	 *            The villager type ID.
-	 * @param handler
-	 *            The handler to register.
-	 */
-	public void addTradeHandler(int villagerID, IVillageTradeHandler handler) {
-		LCLog.debug("Registering trade handler for villager ID " + villagerID);
-		VillagerRegistry.instance().registerVillageTradeHandler(villagerID, handler);
-	}
-
-	/**
-	 * Registers an ore with the ore dictionary.
-	 *
-	 * @param name
-	 *            The ore name.
-	 * @param item
-	 *            The ItemStack.
-	 */
-	public void registerOre(String name, ItemStack item) {
-		LCLog.debug("Registering ore with name " + name);
-		OreDictionary.registerOre(String.format("%s:%s", BuildInfo.modID, name), item);
-	}
-
-	/**
-	 * Creates a new shaped recipe.
-	 *
-	 * @param product
-	 *            The product ItemStack.
-	 * @param params
-	 *            The crafting arrangement.
-	 */
-	public void newRecipe(ItemStack product, Object... params) {
-		LCLog.debug("Registering new generic recipe");
-		GameRegistry.addRecipe(new ShapedOreRecipe(product, params));
-	}
-
-	/**
-	 * Creates a new shapeless recipe.
-	 *
-	 * @param product
-	 *            The product ItemStack.
-	 * @param params
-	 *            The crafting components.
-	 */
-	public void newShapelessRecipe(ItemStack product, Object... params) {
-		LCLog.debug("Registering new generic shapeless recipe");
-		GameRegistry.addRecipe(new ShapelessOreRecipe(product, params));
-	}
-
-	/**
-	 * Registers a new smelting recipe.
-	 *
-	 * @param in
-	 *            The input stack
-	 * @param out
-	 *            The output result
-	 * @param xp
-	 *            Quantity of XP earnt
-	 */
-	public void newSmeltingRecipe(ItemStack in, ItemStack out, float xp) {
-		LCLog.debug("Registering new smelting recipe");
-		FurnaceRecipes.smelting().func_151394_a(in, out, xp);
-	}
-
-	/**
-	 * Registers a new chest item handler.
-	 *
-	 * @param stack
-	 *            The ItemStack to add.
-	 * @param minQty
-	 *            The minimum random item quantity.
-	 * @param maxQty
-	 *            The maximum random item quantity.
-	 * @param weight
-	 *            The weighting of the random number generation for this random.
-	 * @param category
-	 *            The categories of chests this rule applies to.
-	 */
-	public void addRandomChestItem(ItemStack stack, int minQty, int maxQty, int weight, String... category) {
-		WeightedRandomChestContent item = new WeightedRandomChestContent(stack, minQty, maxQty, weight);
-		for (String element : category) {
-			LCLog.debug("Adding new WeightedRandomChestContent for element " + element);
-			ChestGenHooks.addItem(element, item);
-		}
-	}
-
-	/**
-	 * Register a block renderer
-	 *
-	 * @param renderer
-	 *            A block renderer
-	 */
-	public void registerRenderer(ISimpleBlockRenderingHandler renderer) {
-		// TODO: Fix rendering registry :<
-		// renderer.renderID = RenderingRegistry.getNextAvailableRenderId();
-		RenderingRegistry.registerBlockHandler(renderer);
-	}
-
-	/**
 	 * Registers a TileEntity
 	 *
 	 * @param tileType
@@ -354,18 +224,6 @@ public class DefinitionRegistry implements IDefinitionRegistry {
 	 */
 	public void registerTileEntity(Class<? extends LCTile> tileType, String tileName) {
 		GameRegistry.registerTileEntity(tileType, tileName);
-	}
-
-	/**
-	 * Registers a tile entity renderer
-	 *
-	 * @param teClass
-	 *            The tile entity class
-	 * @param renderer
-	 *            The renderer object
-	 */
-	public void addTileEntityRenderer(Class<? extends TileEntity> teClass, Object renderer) {
-		ClientRegistry.bindTileEntitySpecialRenderer(teClass, (TileEntitySpecialRenderer) renderer);
 	}
 
 	/**
