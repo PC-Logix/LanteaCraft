04ad4694a74b780b58784d4dbc86fa2457e689b4
Caitlyn Mainer <Michiyo.Ravencroft@gmail.com>
2014-12-24 22:33:59 -0600

    I did a thing... All stargates are craftable now.

--- src/main/java/pcl/lc/module/ModuleStargates.java
+++ src/main/java/pcl/lc/module/ModuleStargates.java
@@ -145,7 +145,8 @@ public class ModuleStargates implements IModule {
 			Items.transportRingActivator = RegistrationHelper.registerItem(
 					ItemTransportRingActivator.class, "transportRingActivator",
 					CreativeTabHelper.getTab("LanteaCraft: Stargates"));
-
+		
+//MilkyWay Gate
 		RegistrationHelper.newRecipe(
 				new ItemStack(Blocks.stargateRingBlock, 1), "ICI", "NNN",
 				"III", 'I', net.minecraft.init.Items.iron_ingot, 'N',
@@ -179,7 +180,90 @@ public class ModuleStargates implements IModule {
 				net.minecraft.init.Items.redstone, 'c', new ItemStack(
 						ModuleCore.Items.reagentItem, 1,
 						ReagentList.CORECRYSTAL.ordinal()));
+		
+//Pegasus Gate
+		RegistrationHelper.newRecipe(
+				new ItemStack(Blocks.stargateRingBlock, 1, 2), "ICI", "NNN",
+				"III", 'I', net.minecraft.init.Items.iron_ingot, 'N',
+				new ItemStack(ModuleCore.Items.lanteaOreIngot), 'C',
+				new ItemStack(net.minecraft.init.Blocks.cobblestone, 1, 1));
 
+		
+		RegistrationHelper.newRecipe(new ItemStack(Blocks.stargateRingBlock, 1,
+				3), "CgC", "NpN", "IrI", 'I',
+				net.minecraft.init.Items.iron_ingot, 'N', new ItemStack(
+						ModuleCore.Items.lanteaOreIngot), 'C', new ItemStack(
+								net.minecraft.init.Blocks.cobblestone, 1, 1), 'g',
+				net.minecraft.init.Blocks.glowstone, 'r',
+				net.minecraft.init.Items.redstone, 'p',
+				net.minecraft.init.Items.ender_pearl);
+
+		RegistrationHelper.newRecipe(
+				new ItemStack(Blocks.stargateBaseBlock, 1, 4), "CrC", "NeN",
+				"IcI", 'I', net.minecraft.init.Items.iron_ingot, 'N',
+				new ItemStack(ModuleCore.Items.lanteaOreIngot), 'C',
+				new ItemStack(net.minecraft.init.Blocks.cobblestone, 1, 1), 'r',
+				net.minecraft.init.Items.redstone, 'e',
+				net.minecraft.init.Items.ender_eye, 'c', new ItemStack(
+						ModuleCore.Items.reagentItem, 1,
+						ReagentList.CORECRYSTAL.ordinal()));
+		
+
+//Wraith Gate
+		RegistrationHelper.newRecipe(
+				new ItemStack(Blocks.stargateRingBlock, 1, 4), "ICI", "NNN",
+				"III", 'I', net.minecraft.init.Items.iron_ingot, 'N',
+				new ItemStack(ModuleCore.Items.lanteaOreIngot), 'C',
+				new ItemStack(net.minecraft.init.Blocks.mossy_cobblestone, 1, 1));
+
+		
+		RegistrationHelper.newRecipe(new ItemStack(Blocks.stargateRingBlock, 1,
+				5), "CgC", "NpN", "IrI", 'I',
+				net.minecraft.init.Items.iron_ingot, 'N', new ItemStack(
+						ModuleCore.Items.lanteaOreIngot), 'C', new ItemStack(
+								net.minecraft.init.Blocks.mossy_cobblestone, 1, 1), 'g',
+				net.minecraft.init.Blocks.glowstone, 'r',
+				net.minecraft.init.Items.redstone, 'p',
+				net.minecraft.init.Items.ender_pearl);
+
+		RegistrationHelper.newRecipe(
+				new ItemStack(Blocks.stargateBaseBlock, 1, 8), "CrC", "NeN",
+				"IcI", 'I', net.minecraft.init.Items.iron_ingot, 'N',
+				new ItemStack(ModuleCore.Items.lanteaOreIngot), 'C',
+				new ItemStack(net.minecraft.init.Blocks.mossy_cobblestone, 1, 1), 'r',
+				net.minecraft.init.Items.redstone, 'e',
+				net.minecraft.init.Items.ender_eye, 'c', new ItemStack(
+						ModuleCore.Items.reagentItem, 1,
+						ReagentList.CORECRYSTAL.ordinal()));
+
+		
+//Nox Gate
+
+		RegistrationHelper.newRecipe(
+				new ItemStack(Blocks.stargateRingBlock, 1, 6), "ICI", "NNN",
+				"III", 'I', net.minecraft.init.Items.iron_ingot, 'N',
+				new ItemStack(ModuleCore.Items.lanteaOreIngot), 'C',
+				new ItemStack(net.minecraft.init.Blocks.stone, 1, 1));
+
+		
+		RegistrationHelper.newRecipe(new ItemStack(Blocks.stargateRingBlock, 1,
+				7), "CgC", "NpN", "IrI", 'I',
+				net.minecraft.init.Items.iron_ingot, 'N', new ItemStack(
+						ModuleCore.Items.lanteaOreIngot), 'C', new ItemStack(
+								net.minecraft.init.Blocks.stone, 1, 1), 'g',
+				net.minecraft.init.Blocks.glowstone, 'r',
+				net.minecraft.init.Items.redstone, 'p',
+				net.minecraft.init.Items.ender_pearl);
+
+		RegistrationHelper.newRecipe(
+				new ItemStack(Blocks.stargateBaseBlock, 1, 12), "CrC", "NeN",
+				"IcI", 'I', net.minecraft.init.Items.iron_ingot, 'N',
+				new ItemStack(ModuleCore.Items.lanteaOreIngot), 'C',
+				new ItemStack(net.minecraft.init.Blocks.stone, 1, 1), 'r',
+				net.minecraft.init.Items.redstone, 'e',
+				net.minecraft.init.Items.ender_eye, 'c', new ItemStack(
+						ModuleCore.Items.reagentItem, 1,
+						ReagentList.CORECRYSTAL.ordinal()));
 		if (ConfigHelper
 				.getOrSetBooleanParam(
 						config,
