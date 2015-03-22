585142c2787caae359b72940f59c93d559d27943
Lochie <afterlifelochie@afterlifelochie.net>
2015-03-22 19:39:51 +1100

    Add missing generate in DSA.

--- src/main/java/lc/common/crypto/DSAProvider.java
+++ src/main/java/lc/common/crypto/DSAProvider.java
@@ -3,10 +3,12 @@ package lc.common.crypto;
 import java.security.InvalidKeyException;
 import java.security.KeyFactory;
 import java.security.KeyPair;
+import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.security.SecureRandom;
 import java.security.Signature;
 import java.security.SignatureException;
 import java.security.spec.InvalidKeySpecException;
@@ -14,6 +16,12 @@ import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 
 public class DSAProvider {
+
+	public KeyPair generate() throws NoSuchAlgorithmException, NoSuchProviderException {
+		KeyPairGenerator gen = KeyPairGenerator.getInstance("DSA", "SUN");
+		gen.initialize(1024, SecureRandom.getInstance("SHA1PRNG", "SUN"));
+		return gen.generateKeyPair();
+	}
 
 	public byte[] digest(byte[] payload, KeyPair chain) throws InvalidKeyException, SignatureException {
 		try {
