3ec9e4a7a7e5d42bea8414717dd3bc73c987b532
Lochie <afterlifelochie@afterlifelochie.net>
2015-03-29 22:42:36 +1100

    Change GUI handling to be strict

--- src/main/java/lc/common/GUIHandler.java
+++ src/main/java/lc/common/GUIHandler.java
@@ -18,19 +18,18 @@ public class GUIHandler implements IGuiHandler {
 		LCLog.debug("Handling GUI request: id %s", id);
 		IInterfaceRegistry registry = LCRuntime.runtime.registries().interfaces();
 		IInterfaceDefinition def = registry.getDefinition(id);
-		if (def != null)
-			try {
-				String containerClass = def.getContainerClass();
-				LCLog.debug("Creating container %s", containerClass);
-				TileEntity tile = world.getTileEntity(x, y, z);
-				Class<?> container = Class.forName(containerClass);
-				Constructor<?> constr = container
-						.getConstructor(new Class<?>[] { tile.getClass(), EntityPlayer.class });
-				return constr.newInstance(tile, player);
-			} catch (Throwable t) {
-				LCLog.warn("Failed to create container object!", t);
-			}
-		return null;
+		LCLog.doAssert(def != null, "Can't handle server GUI request for element ID %s.", id);
+		try {
+			String containerClass = def.getContainerClass();
+			LCLog.debug("Creating container %s", containerClass);
+			TileEntity tile = world.getTileEntity(x, y, z);
+			Class<?> container = Class.forName(containerClass);
+			Constructor<?> constr = container.getConstructor(new Class<?>[] { tile.getClass(), EntityPlayer.class });
+			return constr.newInstance(tile, player);
+		} catch (Throwable t) {
+			LCLog.warn("Failed to create container object!", t);
+			return null;
+		}
 	}
 
 	@Override
@@ -42,18 +41,18 @@ public class GUIHandler implements IGuiHandler {
 			LCLog.debug("Handling GUI request: id %s", id);
 			IInterfaceRegistry registry = LCRuntime.runtime.registries().interfaces();
 			IInterfaceDefinition def = registry.getDefinition(id);
-			if (def != null)
-				try {
-					String guiClass = def.getGUIClass();
-					LCLog.debug("Creating GUI %s", guiClass);
-					TileEntity tile = world.getTileEntity(x, y, z);
-					Class<?> gui = Class.forName(guiClass);
-					Constructor<?> constr = gui.getConstructor(new Class<?>[] { tile.getClass(), EntityPlayer.class });
-					return constr.newInstance(tile, player);
-				} catch (Throwable t) {
-					LCLog.warn("Failed to create GUI object!", t);
-				}
-			return null;
+			LCLog.doAssert(def != null, "Can't handle client GUI request for element ID %s.", id);
+			try {
+				String guiClass = def.getGUIClass();
+				LCLog.debug("Creating GUI %s", guiClass);
+				TileEntity tile = world.getTileEntity(x, y, z);
+				Class<?> gui = Class.forName(guiClass);
+				Constructor<?> constr = gui.getConstructor(new Class<?>[] { tile.getClass(), EntityPlayer.class });
+				return constr.newInstance(tile, player);
+			} catch (Throwable t) {
+				LCLog.warn("Failed to create GUI object!", t);
+				return null;
+			}
 		}
 	}
 }
