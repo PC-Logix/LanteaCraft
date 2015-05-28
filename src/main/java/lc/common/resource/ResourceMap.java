b6d18c516d87b75008f783fae1fdbae4e1cd92e0
Lochie <afterlifelochie@afterlifelochie.net>
2015-05-28 11:26:08 +1000

    Make add() a return self method, because lazy.

--- src/main/java/lc/common/resource/ResourceMap.java
+++ src/main/java/lc/common/resource/ResourceMap.java
@@ -5,26 +5,28 @@ import java.util.HashMap;
 import net.minecraft.util.ResourceLocation;
 
 public class ResourceMap {
-	
+
 	private final HashMap<String, Object> map = new HashMap<String, Object>();
-	
-	public ResourceMap() { }
-	
-	public void add(String key, ResourceMap map) {
+
+	public ResourceMap() {
+	}
+
+	public ResourceMap add(String key, ResourceMap map) {
 		this.map.put(key, map);
+		return this;
 	}
-	
-	public void add(String key, ResourceLocation location) {
+
+	public ResourceMap add(String key, ResourceLocation location) {
 		this.map.put(key, location);
+		return this;
 	}
-	
+
 	public ResourceMap map(String key) {
 		return (ResourceMap) this.map.get(key);
 	}
-	
+
 	public ResourceLocation resource(String key) {
 		return (ResourceLocation) this.map.get(key);
 	}
-	
 
 }
