9b8955c9241cec9f6a7beaa9b59b0ceb552c0acd
Lochie <afterlifelochie@afterlifelochie.net>
2015-06-24 08:13:31 +1000

    Add GFXBeam stub.

---
+++ src/main/java/lc/client/render/gfx/beam/GFXBeam.java
@@ -0,0 +1,21 @@
+package lc.client.render.gfx.beam;
+
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.World;
+import lc.client.opengl.LCEntityFX;
+
+public class GFXBeam extends LCEntityFX {
+
+	public GFXBeam(World world, double x, double y, double z) {
+		super(world, x, y, z);
+		// TODO Auto-generated constructor stub
+	}
+
+
+	@Override
+	public ResourceLocation getTextureForRender() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
