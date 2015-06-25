91d565162bb0ce032b3776d72295510080eedd15
Lochie <afterlifelochie@afterlifelochie.net>
2015-06-26 09:53:27 +1000

    Add transport ring multi-block goodness.

--- src/main/java/lc/tiles/TileTransportRing.java
+++ src/main/java/lc/tiles/TileTransportRing.java
@@ -4,12 +4,47 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.nbt.NBTTagCompound;
 import cpw.mods.fml.relauncher.Side;
+import lc.LCRuntime;
 import lc.common.base.LCTile;
+import lc.common.base.multiblock.LCMultiblockTile;
+import lc.common.base.multiblock.MultiblockState;
+import lc.common.base.multiblock.StructureConfiguration;
 import lc.common.configuration.xml.ComponentConfig;
 import lc.common.network.LCNetworkException;
 import lc.common.network.LCPacket;
+import lc.common.util.game.BlockFilter;
+import lc.common.util.math.Orientations;
+import lc.common.util.math.Vector3;
 
-public class TileTransportRing extends LCTile {
+public class TileTransportRing extends LCMultiblockTile {
+
+	public final static StructureConfiguration structure = new StructureConfiguration() {
+
+		private final BlockFilter[] filters = new BlockFilter[] {
+				new BlockFilter(LCRuntime.runtime.blocks().frameBlock.getBlock(), 0),
+				new BlockFilter(LCRuntime.runtime.blocks().transporterBlock.getBlock(), 0) };
+
+		@Override
+		public Vector3 getStructureDimensions() {
+			return new Vector3(3, 1, 3);
+		}
+
+		@Override
+		public Vector3 getStructureCenter() {
+			return new Vector3(1, 0, 1);
+		}
+
+		@Override
+		public int[][][] getStructureLayout() {
+			return new int[][][] { { { 0, 0, 0 } }, { { 0, 1, 0 } }, { { 0, 0, 0 } } };
+		}
+
+		@Override
+		public BlockFilter[] getBlockMappings() {
+			return filters;
+		}
+
+	};
 
 	@Override
 	public void configure(ComponentConfig c) {
@@ -18,8 +53,27 @@ public class TileTransportRing extends LCTile {
 	}
 
 	@Override
+	public StructureConfiguration getConfiguration() {
+		return TileTransportRing.structure;
+	}
+
+	@Override
+	public void thinkMultiblock() {
+		if (getState() == MultiblockState.NONE) {
+			if (structure.test(getWorldObj(), xCoord, yCoord, zCoord, null)) {
+				changeState(MultiblockState.FORMED);
+				structure.apply(getWorldObj(), xCoord, yCoord, zCoord, null, this);
+			}
+		} else {
+			if (!structure.test(getWorldObj(), xCoord, yCoord, zCoord, null)) {
+				changeState(MultiblockState.NONE);
+				structure.apply(getWorldObj(), xCoord, yCoord, zCoord, null, null);
+			}
+		}
+	}
+
+	@Override
 	public IInventory getInventory() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
@@ -61,8 +115,7 @@ public class TileTransportRing extends LCTile {
 
 	@Override
 	public String[] debug(Side side) {
-		// TODO Auto-generated method stub
-		return null;
+		return new String[] { String.format("Multiblock: %s", getState()) };
 	}
 
 }
