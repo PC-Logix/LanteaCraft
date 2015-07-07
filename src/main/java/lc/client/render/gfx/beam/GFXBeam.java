75d0b429520bfa845d755ae18bfbe3628543c968
Lochie <afterlifelochie@afterlifelochie.net>
2015-07-08 09:13:30 +1000

    Start beam rendering stuff.

--- src/main/java/lc/client/render/gfx/beam/GFXBeam.java
+++ src/main/java/lc/client/render/gfx/beam/GFXBeam.java
@@ -1,25 +1,69 @@
 package lc.client.render.gfx.beam;
 
+import org.lwjgl.opengl.GL11;
+
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.entity.Entity;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
 import lc.client.opengl.LCEntityFX;
+import lc.common.resource.ResourceAccess;
 import lc.common.util.math.Vector3;
 
 public class GFXBeam extends LCEntityFX {
-	
-	private Vector3 from;
-	private Vector3 to;
 
-	public GFXBeam(World world, Vector3 src, Vector3 dst) {
-		super(world, src.fx(), src.fy(), src.fz());
-		// TODO Auto-generated constructor stub
+	private Object from;
+	private Object to;
+
+	private GFXBeam(World world, Object src, Object dst, boolean solid, float alpha, int maxAge) {
+		super(world, 0, 0, 0);
+		from = src;
+		to = dst;
+		particleRed = particleGreen = particleBlue = 1.0F;
+		motionX = motionY = motionZ = 0.0D;
+		particleGravity = 0.0F;
+		particleAlpha = alpha;
+		particleScale = 1.0f;
+		particleMaxAge = maxAge;
+		noClip = false;
+		setSize(0.01F, 0.01F);
 	}
-
 
 	@Override
 	public ResourceLocation getTextureForRender() {
-		// TODO Auto-generated method stub
-		return null;
+		return ResourceAccess.getNamedResource("textures/fx/beam/beam.png");
+	}
+
+	@Override
+	public void renderParticle(Tessellator tessellator, float frame, float rotX, float rotXZ, float rotZ, float rotYZ,
+			float rotXY) {
+		GL11.glColor4f(1.0F, 1.0F, 1.0F, particleAlpha);
+		Vector3 destination = resolvePosition(to);
+	}
+
+	@Override
+	public int getFXLayer() {
+		return 2;
+	}
+
+	@Override
+	public void onUpdate() {
+		if (this.particleAge++ >= this.particleMaxAge) {
+			this.setDead();
+		}
+		Vector3 origin = resolvePosition(from);
+		posX = origin.fx();
+		posY = origin.fy();
+		posZ = origin.fz();
+	}
+
+	private Vector3 resolvePosition(Object zz) {
+		if (zz instanceof TileEntity)
+			return new Vector3((TileEntity) zz);
+		if (zz instanceof Entity)
+			return new Vector3((Entity) zz);
+		throw new IllegalArgumentException("Unknown or unsupported tracer: " + zz.getClass().getName());
 	}
 
 }
