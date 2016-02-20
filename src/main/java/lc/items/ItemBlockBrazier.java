03ddbbbe4bbb48220ac912ccfb28a6174d30ef73
Lochie <afterlifelochie@afterlifelochie.net>
2016-02-20 15:52:54 +1100

    Fix localization problems, issue #407.

--- src/main/java/lc/items/ItemBlockBrazier.java
+++ src/main/java/lc/items/ItemBlockBrazier.java
@@ -1,6 +1,7 @@
 package lc.items;
 
 import net.minecraft.block.Block;
+import net.minecraft.item.ItemStack;
 import lc.common.base.LCItemBlock;
 import lc.common.configuration.xml.ComponentConfig;
 
@@ -21,6 +22,16 @@ public class ItemBlockBrazier extends LCItemBlock {
 	public void configure(ComponentConfig c) {
 		// TODO Auto-generated method stub
 
+	}
+	
+	@Override
+	public int getMetadata(int i) {
+		return i;
+	}
+
+	@Override
+	public String getUnlocalizedName(ItemStack stack) {
+		return "tile.blockBrazier." + stack.getItemDamage();
 	}
 
 }
